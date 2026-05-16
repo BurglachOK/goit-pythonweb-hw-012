8bdd082b67acf77d4d44d381ea930cc3e00641e3
Serhii <158744795+BurglachOK@users.noreply.github.com>
2026-05-17 01:28:09 +0200

    Revert "fix?"
    This reverts commit 7b224fbcf66033308f124ed5a95fc0961b5ce9a0.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from faker import Faker
 from fastapi import FastAPI, Depends, HTTPException, status, Query, Request, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from typing import List
 from jose import JWTError, jwt
@@ -287,7 +286,5 @@ async def update_avatar(
     return {"avatar_url": avatar_url}
 
 @app.get("/")
-@app.get("")
 def read_root():
-    print({"message": "Welcome to the Premium Contacts API! Made by Serhii Buriak."})
-    return RedirectResponse(url="/docs")
+    return {"message": "Welcome to the Premium Contacts API! Made by Serhii Buriak."}
